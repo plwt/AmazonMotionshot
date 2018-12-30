3ae66fa92f4716d397bc4a6ac1e4f4359819b8c7
Paul Wright <31939113+SeburoSUMO@users.noreply.github.com>
2018-12-30 18:00:36 +0000

    Delete AmazonMotionshot.sh

--- AmazonMotionshot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-adb kill-server
-adb start-server
-adb connect IP.IP.IP.IP
-sleep 5
-adb shell screenrecord --time-limit 10 /sdcard/demo.mp4
-adb pull /sdcard/demo.mp4 /home/Pictures/
-adb shell rm /sdcard/demo.mp4
